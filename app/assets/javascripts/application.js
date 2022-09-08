06ffb8cbe1654e8dcb2067c3e379c4ef8903fe34
MaksimGurov <guromaksim1334@gmail.com>
2022-09-08 15:10:33 +0300

    Change styles main page, add header, top_header, footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,16 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
+//  =require rails-ujs
+//  =require jquery-1.11.0.min
+//  =require bootstrap.min
+//  =require typeahead.bundle
+//  =require jquery.easydropdown
+//  =require easydrop
+//  =require memenu
+//  =require mem
+//  =require responsiveslides.min
+//  =require responsive
+//  =require imagezoom
+//  =require jquery.flexslider
+//  =require flexs
