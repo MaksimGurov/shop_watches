0da5f494455f3480c75bd8df54204398803321e8
MaksimGurov <guromaksim1334@gmail.com>
2022-09-09 20:21:08 +0300

    Add models product, change routes rb, create migrate related_products, views product

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :product, only: [:show]
+
   root to: 'main#index'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_08_085613) do
+ActiveRecord::Schema.define(version: 2022_09_09_122552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(version: 2022_09_08_085613) do
     t.integer "hit", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "related_products", id: false, force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "related_id"
   end
 
 end
