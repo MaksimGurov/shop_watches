d19e0ed08223d2cf0de36f1d971b9491eea23727
MaksimGurov <guromaksim1334@gmail.com>
2022-09-09 20:17:12 +0300

    Add config styles

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,28 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-# Precompile additional assets.
-# application.js, application.scss, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w[bootstrap.scss]
+Rails.application.config.assets.precompile += %w[style.scss]
+Rails.application.config.assets.precompile += %w[memenu.scss]
+Rails.application.config.assets.precompile += %w[flexslider.scss]
+Rails.application.config.assets.precompile += %w[mystyle.scss]
+
+Rails.application.config.assets.precompile += %w[jquery-1.11.0.min.js]
+Rails.application.config.assets.precompile += %w[simpleCart.min.js]
+Rails.application.config.assets.precompile += %w[memenu.js]
+Rails.application.config.assets.precompile += %w[jquery.easydropdown.js]
+Rails.application.config.assets.precompile += %w[responsiveslides.min.js]
+Rails.application.config.assets.precompile += %w[imagezoom.js]
+Rails.application.config.assets.precompile += %w[jquery.flexslider.js]
+Rails.application.config.assets.precompile += %w[bootstrap.min.js]
+Rails.application.config.assets.precompile += %w[glyphicons-halflings-regular.eot]
+Rails.application.config.assets.precompile += %w[glyphicons-halflings-regular.ttf]
+Rails.application.config.assets.precompile += %w[glyphicons-halflings-regular.woff]
+Rails.application.config.assets.precompile += %w[glyphicons-halflings-regular.woff2]
+Rails.application.config.assets.precompile += %w[glyphicons-halflings-regular.svg]
+Rails.application.config.assets.precompile += %w[typeahead.bundle.js]
+Rails.application.config.assets.precompile += %w[easydrop.js]
+Rails.application.config.assets.precompile += %w[mem.js]
+Rails.application.config.assets.precompile += %w[responsive.js]
+Rails.application.config.assets.precompile += %w[flexs.js]
+Rails.application.config.assets.precompile += %w[my.js]
