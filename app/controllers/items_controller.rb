9c868950dc231cbc9fb765a57c6543cd561340f8
MaksimGurov <guromaksim1334@gmail.com>
2022-09-27 09:26:15 +0300

    RSpec post create, invalid attributes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,15 @@
 class ItemsController < ApplicationController
+  include CartsHelper
+  layout false
+
+  def create
+    cart_items.create(item_params)
+  end
+
+  def item_params
+    params.permit(:product_id, :quantity)
+  end
+
+  helper_method :products
+
 end
