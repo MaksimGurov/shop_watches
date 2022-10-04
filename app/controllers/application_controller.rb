c230c4f96b0e8ba7dc2c3c23a465bd61679e92ed
MaksimGurov <guromaksim1334@gmail.com>
2022-10-04 15:20:32 +0300

    Change styles and add method cart_total

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,12 @@ class ApplicationController < ActionController::Base
     current_cart.cart_items
   end
 
-  helper_method :current_cart, :cart_items
+  def cart_total
+    return 0 if cart_items.none?
+
+    cart_items.joins(:product).select('(cart_items.quantity * products.price) as total').sum { |x| x[:total] }
+  end
+
+  helper_method :current_cart, :cart_items, :cart_total
 
 end
