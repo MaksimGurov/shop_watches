bccd012cdaab4e851f7a3bd5e59dc7c368b38c07
MaksimGurov <guromaksim1334@gmail.com>
2022-11-22 17:51:31 +0300

    Render deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :users
-
   resources :product, only: [:show]
-
   resources :category, only: [:show]
-
   resource :cart, only: %i[destroy show] do
     resources :items, only: %i[destroy create]
   end
 
   resources :search, only: [:index]
-
   root to: 'main#index'
 
 end
